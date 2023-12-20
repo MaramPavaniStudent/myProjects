44ee2d2980770a2db717f77a9363ff93a5c30615
M.Pavani <maram.pavani2022@vitstudent.ac.in>
2023-12-20 14:12:43 +0530

    Changed java file

--- firstt.java
+++ firstt.java
@@ -2,5 +2,6 @@ import java.util.Scanner;
 public class firstt {
     public static void main(String[]args){
         System.out.println("this is  the first programme");
+        System.out.println(" i hava made changes ");
     }
 }
